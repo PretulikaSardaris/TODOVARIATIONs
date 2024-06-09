b9649abd9c6f4dd223bd75dcca48e0d7600d9f94
PREETI SARDAR <82765024+PretulikaSardaris@users.noreply.github.com>
2024-06-09 13:13:45 +0200

    add button works!

--- src/Projects/StateTodo.jsx
+++ src/Projects/StateTodo.jsx
@@ -3,27 +3,41 @@ import React, { useState } from 'react';
 // Function declaration
 const StateTodo = () => {
      const [tasks, setTasks ]=useState([]);
+     const [taskInput, setTaskInput ]=useState('');
+
+
 
      const addTask = (task) => {
-     setTasks([...tasks , task])
-        console.log(task)
+      if(taskInput.trim() !== ''){
+        setTasks([...tasks , {text:taskInput , status: 'pending'}]);
+       
+        setTaskInput('')
+      }
+      
      }
   return (
     <div className='flex flex-col items-center justify-center'>
-     <div className='flex gap-10 border-black rounded-md'><input className='bg-slate-100 border-black rounded-md p-5 m-5' type="text" onKeyDown={(e) => {
-   if(e.key === 'Enter') {
-    addTask({Text:e.target.value});
-    e.target.value = ''
-   }
-     }} />
-     <button className='bg-green-600 p-5 m-5 rounded-md' onClick={addTask}>Add</button>
+     <div className='flex gap-10 border-black rounded-md'><input className='bg-slate-100 border-black rounded-md p-5 m-5' type="text" onChange={(e) => {
+     setTaskInput(e.target.value)
+     }} 
+     onKeyDown={(e) => {
+      if(e.key === 'Enter'){
+        addTask()
+      }
+     }}/>
+     <button className='bg-blue-600 p-5 m-5 rounded-md' onClick={addTask}>Add</button>
      </div>
 
      <div className='flex gap-10 p-5 m-5'>
      <ul>
         {tasks.map((task , index) => (
-            <li key={index}>{task.text} 
-            <button>Delete</button></li>
+          <div className='flex gap-10 border-black rounded-md'>
+            <li className='bg-slate-100 border-black rounded-md p-3 mb-2 flex justify-between items-center' key={index}>
+              <span className='bg-slate-100 border-black rounded-md p-5 m-5 font-bold text-black text-xl'>{task.text} </span>
+            
+           <div className='flex flex-row gap-5' key={index}><button className='bg-green-600 p-5 m-5 rounded-md'>Done</button>
+           <button className='bg-purple-600 p-5 m-5 rounded-md'>Pending</button>
+           <button className='bg-red-600 p-5 m-5 rounded-md'>Delete</button></div> </li></div>
         ))}
      </ul>
      </div>
